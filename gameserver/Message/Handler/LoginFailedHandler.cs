513c00eb31f6d62a45f1fa5bd9c28b059ac41311
arda64 <166497625+arda6464@users.noreply.github.com>
2025-10-02 00:29:15 +0300

    other update

--- gameserver/Message/Handler/LoginFailedHandler.cs
+++ gameserver/Message/Handler/LoginFailedHandler.cs
@@ -6,13 +6,13 @@ public static class Loginfailed
     public static void Send(Session session, string erormessage, int erorid)
     {
         ByteBuffer buffer = new ByteBuffer();
-        buffer.WriteInt((int)MessageType.Loginfailed);
+        buffer.WriteInt((int)MessageType.LoginFailed);
         buffer.WriteInt(erorid);
         buffer.WriteString(erormessage);
         byte[] veri = buffer.ToArray();
         buffer.Dispose();
         session.Send(veri);
         session.Close();
-        Console.WriteLine("send loginfailed");
+        Console.WriteLine("send loginfailed sebep: " + erormessage);
     }
 }
--- gameserver/Network/Session.cs
+++ gameserver/Network/Session.cs
@@ -15,18 +15,50 @@ public class Session
         this.stream = client.GetStream();
     }
     public void Start()
+{
+    byte[] buffer = new byte[4096];
+
+    try
     {
-        byte[] buffer = new byte[4096];
         while (true)
         {
-            int bytesRead = stream.Read(buffer, 0, buffer.Length);
-            if (bytesRead <= 0)
+            int bytesRead = 0;
+            try
+            {
+                bytesRead = stream.Read(buffer, 0, buffer.Length);
+            }
+            catch (ObjectDisposedException)
+            {
+                Console.WriteLine("[Session] Stream kapalı, client çıkış yaptı.");
                 break;
+            }
+            catch (IOException)
+            {
+                Console.WriteLine("[Session] Client bağlantısı kesildi.");
+                break;
+            }
+
+            if (bytesRead <= 0)
+            {
+                Console.WriteLine("[Session] Client bağlantıyı kapattı.");
+                break;
+            }
+
+        
             MessageManager.HandleMessage(this, buffer);
         }
     }
+    finally
+    {
+        Close();
+    }
+}
     public void Send(byte[] buffer)
     {
+        if (!client.Connected)
+        {
+            throw new InvalidOperationException("Bağlantı kapalı");
+        }
         try
         {
 
