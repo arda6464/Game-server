72e36ee2d2a578578e5853e54348862fb70a5009
arda64 <166497625+arda6464@users.noreply.github.com>
2025-10-29 11:18:07 +0300

    matchmaking system add

---
+++ gameserver/Battle/ArenaManager.cs
@@ -0,0 +1,60 @@
+public static class ArenaManager
+{
+    private static readonly Dictionary<int, List<Player>> arenas = new();
+    private static int nextArenaId = 1;
+
+    public static int CreateArena()
+    {
+        int id = nextArenaId++;
+        arenas[id] = new List<Player>();
+        Logger.battlelog($"Yeni harita oluşturuldu id: {id}");
+        return id;
+    }
+    public static void AddPlayer(int arenaid, Player player)
+    {
+        if (arenas.TryGetValue(arenaid, out var players))
+        {
+            players.Add(player);
+            Logger.battlelog($"{player.Username} {arenaid} arenasına katıldı!");
+        }
+    }
+    
+    public static bool RemovePlayer(int arenaId, string accountId)
+    {
+        if (!arenas.TryGetValue(arenaId, out var players))
+        {
+            Logger.battlelog($"[ARENA] Uyarı: Arena {arenaId} bulunamadı!");
+            return false;
+        }
+
+        var player = players.FirstOrDefault(p => p.AccountId == accountId);
+        if (player != null)
+        {
+            players.Remove(player);
+            Logger.battlelog($"[ARENA] {accountId} arenadan çıkarıldı. Kalan: {players.Count}");
+            return true;
+        }
+
+        return false;
+    }
+    
+    public static void RemoveArena(int arenaId)
+    {
+        if (arenas.Remove(arenaId))
+        {
+            Logger.battlelog($"[ARENA] Arena {arenaId} silindi.");
+        }
+    }
+    
+    public static List<Player> GetPlayers(int arenaId)
+    {
+        if (!arenas.TryGetValue(arenaId, out var players))
+        {
+            Logger.battlelog($"[ARENA] Uyarı: Arena {arenaId} bulunamadı!");
+            return new List<Player>();
+        }
+
+        Logger.battlelog($"[ARENA] GetPlayers({arenaId}) çağrıldı, {players.Count} oyuncu var.");
+        return players;
+    }
+}
---
+++ gameserver/Battle/MatchMaking.cs
@@ -0,0 +1,117 @@
+using System.Numerics;
+
+public static class MatchMaking
+{
+    public static readonly List<Session> waitingQueue = new();
+    private static readonly object lockObj = new();
+    private const int PlayersPerMatch = 2;
+
+    public static void JoinQueue(Session session)
+    {
+        lock (lockObj)
+        {
+            if (waitingQueue.Contains(session))
+                return;
+
+            waitingQueue.Add(session);
+            Console.WriteLine($"{session.PlayerData?.Username} matchmaking'e katıldı. Toplam: {waitingQueue.Count}");
+            foreach(var player in waitingQueue)
+            {
+                ByteBuffer buffer = new ByteBuffer();
+                buffer.WriteInt((int)MessageType.MatchMakingAddPlayer);
+                buffer.WriteShort(1);
+                byte[] bytes = buffer.ToArray();
+                buffer.Dispose();
+                player.Send(bytes);
+            }
+
+            if (waitingQueue.Count >= PlayersPerMatch)
+            {
+                
+                StartMatch();
+            }
+        }
+    }
+
+
+    private static void StartMatch()
+    {
+
+        List<Session> players = waitingQueue.Take(PlayersPerMatch).ToList();
+        waitingQueue.RemoveRange(0, PlayersPerMatch);
+
+        int arenaId = ArenaManager.CreateArena();
+        Console.WriteLine($"Match başladı! Arena ID: {arenaId}");
+        Vector2[] spawnPoints =
+    {
+        new Vector2(-3, 0),
+        new Vector2(3, 0),
+        new Vector2(0, 3),
+        new Vector2(0, -3)
+    };
+
+        int spawnIndex = 0;
+        foreach (var session in players)
+        {
+             var spawn = spawnPoints[spawnIndex % spawnPoints.Length];
+            spawnIndex++;
+            var player = new Player
+            {
+                AccountId = session?.PlayerData?.AccountId,
+                Username = session?.PlayerData?.Username ?? "No Name",
+                Health = 100,
+                Position = new Vector2(spawn.X, spawn.Y),
+                ArenaId = arenaId,
+                session = session
+
+            };
+            session.PlayerData = player;
+
+           ArenaManager.AddPlayer(arenaId, player);
+
+        }
+
+          var allplayers = ArenaManager.GetPlayers(arenaId);
+            //client to send is matchfound packets:
+           foreach (var session in players)
+{
+    ByteBuffer buffer = new ByteBuffer();
+    buffer.WriteInt((int)MessageType.MatchFound);
+    buffer.WriteInt(allplayers.Count);
+
+    foreach (var p in allplayers)
+    {
+        buffer.WriteString(p.AccountId);
+        buffer.WriteString(p.Username);
+        buffer.WriteFloat(p.Position.X);
+        buffer.WriteFloat(p.Position.Y);
+    }
+
+    session.Send(buffer.ToArray());
+    buffer.Dispose();
+}
+    }
+    public static void RemoveQueue(Session session)
+    {
+        if (waitingQueue.Contains(session))
+        {
+            waitingQueue.Remove(session);
+            Console.WriteLine($"{session?.PlayerData?.Username} kuyruktan kaldırıldı!");
+            foreach(var player in waitingQueue)
+            {
+                ByteBuffer buffer = new ByteBuffer();
+                buffer.WriteInt((int)MessageType.MatchMakingAddPlayer);
+                buffer.WriteShort(1);
+                byte[] bytes = buffer.ToArray();
+                buffer.Dispose();
+                player.Send(bytes);
+            }
+            
+        }
+        else  Console.WriteLine($"{session?.PlayerData?.Username} kuyrukta değilki?!");
+
+    }
+    
+
+    
+}
