0df20782cc0bc6db61f32450577eeed7b016d1bb
arda64 <166497625+arda6464@users.noreply.github.com>
2025-12-13 02:55:48 +0300

    Create LoginOK.cs

---
+++ gameserver/Message/Handler/Auth/LoginOK.cs
@@ -0,0 +1,16 @@
+public static class LoginOK
+{
+    public static void Handle(Session session, string newtoken,string newid)
+    {
+        var account = AccountManager.LoadAccount(session.AccountId);
+        ByteBuffer buffer = new ByteBuffer();
+        buffer.WriteInt((int)MessageType.LoginOKResponse);
+        buffer.WriteString(newtoken);
+        buffer.WriteString(newid);
+        byte[] response = buffer.ToArray();
+        buffer.Dispose();
+        session.Send(response);
+
+        // napcaz ki yaaa 
+    }
+}
